28b6ae0f714f5e0f9a380d657b21d1b2b9914976
Mike <wellwind@mail2000.com.tw>
2022-07-25 23:13:19 +0800

    任務 06：Reactive Forms 繫結表單資料

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS')
+    ])
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  addTag(tag: string): void {
+    this.post.controls.tags.push(this.formBuilder.control(tag));
+  }
+
+  removeTag(index: number): void {
+    this.post.controls.tags.removeAt(index);
+  }
+
+  createPost(): void {
+    console.log(this.post.value);
+  }
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateComponent } from './create/create.component';
+import { PostComponent } from './post/post.component';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
-import { PostComponent } from './post/post.component';
-import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { CreateComponent } from './create/create.component';
   ],
   imports: [
     CommonModule,
-    PostsRoutingModule
+    PostsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class PostsModule { }
