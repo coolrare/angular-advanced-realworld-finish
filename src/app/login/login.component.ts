8ed7f817d51fab09acd4bbfa637e590eff544e54
Mike <wellwind@mail2000.com.tw>
2022-07-25 22:33:28 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+import { UserLoginInfo } from '../interfaces/login-info';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: UserLoginInfo = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.loginService.login(this.user).pipe(
+      catchError((error: HttpErrorResponse) => {
+        alert(error.message);
+        throw error;
+      })
+    ).subscribe((result) => {
+      localStorage.setItem('token', result.user.token);
+      this.router.navigateByUrl('/');
+    });
   }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
@@ -11,7 +12,8 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
